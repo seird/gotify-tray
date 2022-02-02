89c22fd1fbad97a8eb0082aefae87ba1e6e3a6a1
dries.k <k.dries@protonmail.com>
2022-02-02 17:37:49 +0100

    log tray/notification capabilities

--- gotify_tray/gui/Tray.py
+++ gotify_tray/gui/Tray.py
@@ -1,10 +1,20 @@
+import logging
+
 from PyQt6 import QtGui, QtWidgets
 from gotify_tray.__version__ import __title__
+
+
+logger = logging.getLogger("gotify-tray")
 
 
 class Tray(QtWidgets.QSystemTrayIcon):
     def __init__(self):
         super(Tray, self).__init__()
+
+        if not self.isSystemTrayAvailable():
+            logger.warning("System tray is not available.")
+        if not self.supportsMessages():
+            logger.warning("System does not support notifications.")
 
         self.set_icon_error()
         self.setToolTip(__title__)

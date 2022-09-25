03347648dbd06e0ff2df6d8b1226c1002092db1a
dries.k <k.dries@protonmail.com>
2022-09-25 10:41:49 +0200

    v0.2.1

--- gotify_tray/__version__.py
+++ gotify_tray/__version__.py
@@ -3,4 +3,4 @@ __description__ = (
     "A tray notification application for receiving messages from a Gotify server."
 )
 __url__ = "https://github.com/seird/gotify-tray"
-__version__ = "0.2.0"
+__version__ = "0.2.1"
--- version.py
+++ version.py
@@ -6,8 +6,8 @@ VSVersionInfo(
     ffi=FixedFileInfo(
         # filevers and prodvers should be always a tuple with four items: (1, 2, 3, 4)
         # Set not needed items to zero 0.
-        filevers=(0, 2, 0, 0),
-        prodvers=(0, 2, 0, 0),
+        filevers=(0, 2, 1, 0),
+        prodvers=(0, 2, 1, 0),
         # Contains a bitmask that specifies the valid bits 'flags'r
         mask=0x3F,
         # Contains a bitmask that specifies the Boolean attributes of the file.
@@ -34,12 +34,12 @@ VSVersionInfo(
                         StringStruct(u"Comments", u"Gotify Tray"),
                         StringStruct(u"CompanyName", u""),
                         StringStruct(u"FileDescription", u"Gotifiy Tray"),
-                        StringStruct(u"FileVersion", u"0.2.0"),
+                        StringStruct(u"FileVersion", u"0.2.1"),
                         StringStruct(u"InternalName", u"gotify-tray"),
                         StringStruct(u"LegalCopyright", u""),
                         StringStruct(u"OriginalFilename", u"gotify-tray.exe"),
                         StringStruct(u"ProductName", u"Gotify Tray"),
-                        StringStruct(u"ProductVersion", u"0.2.0"),
+                        StringStruct(u"ProductVersion", u"0.2.1"),
                     ],
                 )
             ]
